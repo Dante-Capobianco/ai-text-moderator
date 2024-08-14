751ff5dec822b29fdc6a4e7e4689e8f574de3378
Dante Capobianco <dante.capobianco@mail.utoronto.ca>
2024-08-14 19:55:05 -0400

    Update tokenizer.py

--- socialMediaClassificationTransformer/data/tokenizer.py
+++ socialMediaClassificationTransformer/data/tokenizer.py
@@ -1,1 +1,2 @@
 #Tokenize text input (given an input set of data based on training/trainer.py)
+#Put results of different methods (different ways of handling padding, truncation, long sequences) & distribution of token lenghts in Jupyter notebook & adjust maximum sequence lengths
