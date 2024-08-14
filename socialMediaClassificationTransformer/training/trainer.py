a1bcc8d8cce680ba1cde639ad9eb686c26ae714f
Dante Capobianco <dante.capobianco@mail.utoronto.ca>
2024-08-13 21:49:54 -0400

    Update trainer.py

--- socialMediaClassificationTransformer/training/trainer.py
+++ socialMediaClassificationTransformer/training/trainer.py
@@ -1,2 +1,2 @@
-#Training loop, including batching, loss calculation, and backpropagation using the optimizer to update weights
+#Training loop, including batching, loss calculation, backpropagation using the optimizer to update weights, evaluating the model on validation data every 1+ epochs
 #Saves model as well
