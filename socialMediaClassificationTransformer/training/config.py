ab26548c90bae553dc2b767e9787f1b60afcd148
Dante Capobianco <dante.capobianco@mail.utoronto.ca>
2024-08-14 17:15:29 -0400

    Update config.py

--- socialMediaClassificationTransformer/training/config.py
+++ socialMediaClassificationTransformer/training/config.py
@@ -1,4 +1,4 @@
 #Store hyperparameters (learning rate, batch size, epochs, number of layers)
-DATA_PATH = "data/datasets.py"
+DATA_PATH = "data/datasets/"
 TOKENIZER_PATH = "data/tokenizer.py"
 SAVED_MODEL_PATH = "trained_models/"
