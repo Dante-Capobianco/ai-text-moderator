aa77314f1ec230d5bbf0cd4ae8e42c297370de2e
Dante Capobianco <dante.capobianco@mail.utoronto.ca>
2024-08-13 21:38:59 -0400

    Update transformer.py

--- socialMediaClassificationTransformer/models/transformer.py
+++ socialMediaClassificationTransformer/models/transformer.py
@@ -1,2 +1,1 @@
 #Developing architecture, combining each component in the structure
-# adding residual connections & layer normalization before each attention mechanism & FNN, and before final classification head
