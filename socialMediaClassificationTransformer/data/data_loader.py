d690f34e944db4ca7d64c6e33cd9a93783ac6764
Dante Capobianco <dante.capobianco@mail.utoronto.ca>
2024-08-14 18:20:15 -0400

    Update data_loader.py

--- socialMediaClassificationTransformer/data/data_loader.py
+++ socialMediaClassificationTransformer/data/data_loader.py
@@ -1,1 +1,1 @@
-#Load raw data, initial preprocessing (ie filtering missing/unncessary data), split into training, validation, and testing (with balance between all labels)
+#Load raw data, initial preprocessing (ie filtering missing/unncessary data, lowercasing, remove special char), split into training, validation, and testing (with balance between all labels)
