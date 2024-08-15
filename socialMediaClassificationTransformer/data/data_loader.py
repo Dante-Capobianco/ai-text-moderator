1bba451314c41d06021b50d8c66ad2bb2136089a
Dante Capobianco <dante.capobianco@mail.utoronto.ca>
2024-08-15 07:42:21 -0400

    Update data_loader.py

--- socialMediaClassificationTransformer/data/data_loader.py
+++ socialMediaClassificationTransformer/data/data_loader.py
@@ -1,2 +1,7 @@
 #Load raw data, initial preprocessing (ie filtering missing/unncessary data, lowercasing, remove special char), visualize distribution of labels & results of preprocessing techniques in Jupyter notebook, split into training, validation, and testing (with balance between all labels)
 #Store processed data in pkl
+
+
+balance avg char per comment, number of each label
+
+check if multiple whitepaces and convert to 1
