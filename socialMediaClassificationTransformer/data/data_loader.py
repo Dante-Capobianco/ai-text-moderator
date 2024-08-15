a7a05502807a4f6bee4225b4177f7e9b6946f792
Dante Capobianco <dante.capobianco@mail.utoronto.ca>
2024-08-15 16:49:58 -0400

    Update data_loader.py

--- socialMediaClassificationTransformer/data/data_loader.py
+++ socialMediaClassificationTransformer/data/data_loader.py
@@ -2,6 +2,20 @@
 #Store processed data in pkl
 
 
-balance avg char per comment, number of each label
+#balance avg char per comment, number of each label
 
-check if multiple whitepaces and convert to 1
+
+#PULL THIS INTO LOCAL REPO
+
+import csv
+import re
+
+def clean_text(message):
+    message = message.replace('"""', '"')
+    message = message.lower()
+    # Replace multiple white spaces with a single space
+    message = re.sub(r'\s+', ' ', message)
+    
+    return message.strip()
+    
+#check if message is empty or zero non-whitespace char  to skip then
